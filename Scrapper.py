a6b5ceb82e74e98891f1d69898398b398ac3ebac
UppercutsFromKupchino <mitya.kalmykov.01@mail.ru>
2022-07-27 12:43:47 +0300

    Получен список ссылок на объекты

--- Scrapper.py
+++ Scrapper.py
@@ -7,17 +7,26 @@ class Scrapper:
     def __init__(self):
         self.string = AddressString.AddressString()
         self.string.replace_rus_to_eng()
+        self.get_children_elements()
 
     def get_soup(self):
         # Создание супа
         response = requests.get(self.string.string, headers={'user-agent': 'YaBrowser/22.7.1.806'})
         soup = BeautifulSoup(response.text, 'html5lib')
-        divs = soup.find_all('div', class_='c90a25d457')
+        divs = soup.find_all('div', class_='c90a25d457')  # Список всех дивов, содержащих в себе нужные ссылки
+        return divs
 
-        # Поиск всех тегов <a>
-        children = []
+    def get_children_elements(self):
+        self.refs = []
+        divs = self.get_soup()
         for div in divs:
-            children.append(div.findChildren('a', recursive=False))
-        print(children)
+            self.refs.append(div.find('a').get('href'))  # Список ссылок
+
+    # def
+        # Поиск всех тегов <a>
+        # children = []
+        # for div in divs:
+        #     children.append(div.findChildren('a', recursive=False))
+        # print(children)
         # with open("govno.txt", "w", encoding="UTF-8") as file:
         #     file.write(soup.prettify())
--- main.py
+++ main.py
@@ -3,4 +3,3 @@ import Scrapper
 
 if __name__ == '__main__':
     scrapper = Scrapper.Scrapper()
-    scrapper.get_soup()
